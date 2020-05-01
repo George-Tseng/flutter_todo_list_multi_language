e1e0f6c0addc72924fb58778640457071def1ac8
George-Tseng <georgecycuphy@cycu.org.tw>
2020-05-01 09:15:34 +0800

    簡化資料定義寫法
    砍掉setter&getter

--- lib/dbDatas/dbDatas.dart
+++ lib/dbDatas/dbDatas.dart
@@ -1,50 +1,29 @@
 class DBDatas {
-  int _id; //id
-  String _task; //項目
-  String _date; //日期
-  String _time; //時間
-  String _status; //狀態
+  int id; //id
+  String task; //項目
+  String date; //日期
+  String time; //時間
+  String status; //狀態
 
-  DBDatas(this._id, this._task, this._date, this._time, this._status);
-
-  //getter
-  int get id => _id;
-  String get task => _task;
-  String get date => _date;
-  String get time => _time;
-  String get status => _status;
-  //getter
-
-  //setter
-  set id(int newId) => this._id = newId;
-  set task(String newTask) {
-    if (newTask.length <= 255) {
-      this._task = newTask;
-    }
-  }
-
-  set date(String newDate) => this._date = newDate;
-  set time(String newTime) => this._time = newTime;
-  set status(String newStatus) => this._status = newStatus;
-  //setter
+  DBDatas(this.id, this.task, this.date, this.time, this.status);
 
   //將資料轉成map
   Map<String, dynamic> toMap() {
-    var map = Map<String, dynamic>();
-    if (id != null) map['id'] = _id;
-    map['task'] = _task;
-    map['date'] = _date;
-    map['time'] = _time;
-    map['status'] = _status;
+    Map<String, dynamic> map = Map<String, dynamic>();
+    if (id != null) map['id'] = id;
+    map['task'] = task;
+    map['date'] = date;
+    map['time'] = time;
+    map['status'] = status;
     return map;
   }
 
   //將資料從map轉回來
   DBDatas.fromMapObject(Map<String, dynamic> map) {
-    this._id = map['id'];
-    this._task = map['task'];
-    this._date = map['date'];
-    this._time = map['time'];
-    this._status = map['status'];
+    this.id = map['id'];
+    this.task = map['task'];
+    this.date = map['date'];
+    this.time = map['time'];
+    this.status = map['status'];
   }
 }
